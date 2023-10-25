79413949b955649902d86fa65438e0032b1d5349
Maisha Shayree <mshayree@ivytech.edu>
2023-10-25 14:33:29 -0400

    new

---
+++ classwork/events.js
@@ -0,0 +1,142 @@
+function oldschool(){
+    document.getElementById('htmlEH').innerHTML="This is bad practisr do not use thi one";
+}
+
+function tradText(){
+    document.getElementById('traditional').innerHTML="You can only attach one function to this event.";
+} 
+let tradHandler = document.getElementByTagName('input')
+{1};
+
+tradHandler.onclick=tradText;
+
+
+function level2Text(){
+    document.getElementById('level2').innerHTML="Use this. You can attach multiple functions to an event";
+}
+
+function alertMe(){
+    alert("This is part of the level 2 funcationality");
+}
+let level2Btn=documnet.getElementById('input')
+{2};
+level2Btn.addEventListerner('click', function(){
+    level2Text();
+    alertMe();
+});
+//UI events
+
+let pURL=document.getElementById('pageURL');
+let Update=document.getElementById('myLastUpdate');
+
+function displayInfo(){
+    pURL.innerHTML=window.location;
+    Update.innerHTML = "LastUpdate" + document.lastModified;
+}
+
+window.addEventListener('load',displayInfo);
+
+//Keyboard events
+
+let keyNum=documnet.getElementById('pressNumber');
+let keyInput=documnet.getElementByIdTagName('input')[3];
+let pressCount=0;
+function countKey(){
+    pressCount++;
+    keyNum.innerHTML= "You have pressed a key"+pressCount+ "times";
+
+}
+keyInput.addEventListener('keyPress',countKey);
+
+//Mouse Events
+let dangerTxt=document.getElementById('danger');
+
+function changeDanger(){
+    dangerTxt.innerHTML= "<br>YOu Changed the Text. Wat to go!</br>";
+
+}
+dangerTxt.addEventListener('dbclick',changeDanger);
+
+//focus events
+let blurText= documnt.getElementsByClassName('blurText')[0];
+
+function change2Upper(){
+    blurText.style.textTransforms="uppercase";
+}
+
+blurText.addEventListener('blur',change2Upper);
+
+//Form events
+let dropD=document.getElementById('js');
+let selectOutput= document.getElementById('selectText');
+
+function ddChoice(){
+    let yourChoice =dropD.options[dropD.selectedIndex].value;
+    let choiceText;
+    switch(yourChoice){
+        case"javascript":
+        choiceText="javascript is the base of all of these";
+        break;
+        case "jQuery":
+            choiceText="You will learn jQuary"
+            break;
+        case "Angular":
+            choiceText="You amy or may not learn ANgular in an IUPUI course";
+            break;
+        case "React":
+            choiceText="React is very popular";
+            break;
+        default:
+            choiceText="what did you choose";
+            break;
+
+    }
+
+    selectOutput.innerHTML= choiceText;
+}
+ dropD.addEventListerner('change',ddChoice);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- homework-five/script.js
+++ homework-five/script.js
@@ -1,5 +1,5 @@
 function startGame() {
-    let player = prompt("Welcome to my choose-your-own adventure game. Please start by entering your name");
+    let player = prompt("Welcome to the choose-your-own adventure game. Please start by entering your name");
     alert("Welcome, " + player + "! Today we are going on a campus adventure. Are you ready to begin?");
     let tool = prompt("To begin your adventure, you need to select a tool. Please pick your tool of choice: ladder, flashlight, or shovel");
     let decision1 = prompt("You are walking through the campus center and hear a menacing growl coming from another room. Do you want to enter (1) or leave (2)?");
@@ -12,13 +12,22 @@ function startGame() {
             let decision3 = prompt("Behind the blue door, you find a riddle. Solve it (1) or ignore it and keep exploring (2)?");
             if (decision3 === "1") {
                 alert("You solve the riddle and gain access to a secret library with valuable knowledge. Well done!");
-            } else {
+            } else if (decision3 === "2") {
                 alert("You ignore the riddle and continue exploring. You find a map leading to hidden treasures!");
+            } else {
+                alert("Invalid choice. You stand there and nothing happens. The adventure ends.");
             }
         } else if (decision2 === "3") {
             let decision4 = prompt("You step into a portal behind the green door. It leads to a magical forest. Explore (1) or turn back (2)?");
             if (decision4 === "1") {
-                alert("You explore the magical forest and befriend a mythical creature. You both embark on a legendary quest!");
+                let decision5 = prompt("In the magical forest, you encounter a mystical creature. Befriend it (1) or run away (2)?");
+                if (decision5 === "1") {
+                    alert("You befriend the creature and it grants you magical powers. You become the hero of the forest!");
+                } else if (decision5 === "2") {
+                    alert("You run away in fear. As you exit the forest, you find a hidden path to a different adventure.");
+                } else {
+                    alert("Invalid choice. The mystical creature disappears, leaving you in awe. The adventure continues.");
+                }
             } else {
                 alert("You turn back from the magical forest and find a hidden cave with ancient artifacts. Great discovery!");
             }
@@ -26,21 +35,18 @@ function startGame() {
             alert("Invalid choice. You stand there and nothing happens. The adventure ends.");
         }
     } else if (decision1 === "2") {
-        alert("You decide to leave the campus center. As you walk away, you find a hidden path leading to a magical forest. You venture into the forest and begin a new adventure.");
+        let decision6 = prompt("As you leave, you find a mysterious map on the ground. Follow the map (1) or ignore it and keep walking (2)?");
+        if (decision6 === "1") {
+            alert("You follow the map's clues and discover a hidden temple. Inside, you find a legendary artifact. You win!");
+        } else if (decision6 === "2") {
+            alert("You ignore the map and continue walking. Suddenly, you stumble upon a portal to another dimension. The adventure intensifies!");
+        } else {
+            alert("Invalid choice. The map fades away, leaving you with a sense of curiosity. The adventure continues.");
+        }
     } else {
         alert("Invalid choice. The adventure ends.");
     }
 }
 
-
-
 // Call the startGame() function to begin the adventure
 startGame();
-document.getElementById("startGame").addEventListener("click", startGame);
-
-         
-        
-
-
-
-
